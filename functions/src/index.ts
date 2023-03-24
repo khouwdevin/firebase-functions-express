d6485218fa6025365b3034ea4bf7faf081334c23
khouwdevin <khouwdevin@gmail.com>
2023-03-24 15:24:13 +0700

    update index ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,18 +2,18 @@ import './pre-start'; // Must be the first import
 
 import * as functions from "firebase-functions";
 
-import app from './server';
+import server from './server';
 import EnvVars from './declarations/major/EnvVars';
 import { NodeEnvs } from './declarations/enums';
 import jetLogger from 'jet-logger';
 
 if (EnvVars.nodeEnv === NodeEnvs.Dev) {
   const msg = ('Express server started on port: ' + EnvVars.port.toString());
-  app.listen(EnvVars.port, () => jetLogger.info(msg));
+  server.listen(EnvVars.port, () => jetLogger.info(msg));
 }
 
 // **** Export default **** //
 
-export const server = functions
+export const app = functions
   .region("us-east1")
-  .https.onRequest(app);
+  .https.onRequest(server);
